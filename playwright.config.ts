3d557b2cbc7c80b430762594c6cc2efa384c8896
Jack Zhao <bugzpodder@hotmail.com>
2023-09-24 17:48:28 +0000

    remove other browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,24 +4,6 @@ export default defineConfig({
   expect: {
     timeout: 30 * 1000,
   },
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-    {
-      name: "Mobile Chrome",
-      use: { ...devices["Pixel 5"] },
-    },
-    {
-      name: "Mobile Safari",
-      use: { ...devices["iPhone 12"] },
-    },
-  ],
   use: {
     baseURL: "http://localhost:8000/",
   },
