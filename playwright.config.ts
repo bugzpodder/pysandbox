9db3e5480965cd4903afa4ccb9469aa7d789aa78
Jack Zhao <bugzpodder@hotmail.com>
2023-09-24 19:29:58 +0000

    skip all worker tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,10 +6,11 @@ export default defineConfig({
   },
   use: {
     baseURL: "http://localhost:8000/",
+    trace: "on-first-retry",
   },
   webServer: {
     command: "yarn serve",
-    reuseExistingServer: true,
+    reuseExistingServer: !process.env.CI,
     url: "http://localhost:8000/",
   },
   workers: process.env.CI ? 1 : undefined,
